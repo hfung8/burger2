6066e28204f761876bb2651d8067fc8f306059d7
hfung8 <harrison.hok.fung@gmail.com>
2017-03-31 08:41:33 -0400

    made changes to the styling of the webpage

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var methodOverride = require('method-override');
 var app = express();
 var PORT = process.env.PORT || 3306;
 
+
 app.use(bodyParser.urlencoded({
 	extended: false
 }));
@@ -13,6 +14,7 @@ var exphbs = require('express-handlebars');
  
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+app.use(express.static(__dirname + "/public"));
 
 require("./routes/api-routes.js")(app);
 var db = require("./models");
